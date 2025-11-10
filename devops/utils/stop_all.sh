bf080dc3de5f310c60a59471fc216b5dc3752f0a
C-Tronik Studio <ivan.rovano@outlook.com>
2025-11-10 19:39:45 +0100

    Delete devops directory

--- devops/utils/stop_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ps axco pid,command | grep -i dotnet | awk '{ print $1; }' | xargs kill -9 2>/dev/null
-ps axco pid,command | grep -i python | awk '{ print $1; }' | xargs kill -9 2>/dev/null
